c8a06e5b94bb216abe1e1839d925b62f806e577d
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-07-30 22:31:05 +0300

    Bind Factories
    1-Board script updated

--- Assets/Scripts/Game/Installers/GamePlayInstaller.cs
+++ Assets/Scripts/Game/Installers/GamePlayInstaller.cs
@@ -5,8 +5,10 @@ using Zenject;
 public class GamePlayInstaller : ScriptableObjectInstaller<GamePlayInstaller>
 {
     [SerializeField] private Cell cellPrefab;
+    [SerializeField] private ItemBase itemPrefab;
     public override void InstallBindings()
     {
         Container.BindFactory<Cell,Cell.CellFactory>().FromComponentInNewPrefab(cellPrefab).AsSingle();
+        Container.BindFactory<ItemBase, ItemBase.ItemBaseFactory>().FromComponentInNewPrefab(itemPrefab).AsSingle();
     }
 }
