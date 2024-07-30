e52087dcd0587b9186edc05332822c516326285b
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-07-30 22:17:36 +0300

    Updated board and GamePlayInstaller

--- Assets/Scripts/Game/BoardBase/Cell.cs
+++ Assets/Scripts/Game/BoardBase/Cell.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 public class Cell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CellFactory : PlaceholderFactory<Cell>
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/Game/Installers/GamePlayInstaller.cs
+++ Assets/Scripts/Game/Installers/GamePlayInstaller.cs
@@ -4,7 +4,9 @@ using Zenject;
 [CreateAssetMenu(fileName = "GamePlayInstaller", menuName = "Installers/GamePlayInstaller")]
 public class GamePlayInstaller : ScriptableObjectInstaller<GamePlayInstaller>
 {
+    [SerializeField] private Cell cellPrefab;
     public override void InstallBindings()
     {
+        Container.BindFactory<Cell,Cell.CellFactory>().FromComponentInNewPrefab(cellPrefab).AsSingle();
     }
 }
