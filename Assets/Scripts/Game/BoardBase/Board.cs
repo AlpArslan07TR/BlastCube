bcb292740f66e0f27c31f975abc4779159cd5573
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-08-05 21:42:38 +0300

    added get neighbour from direaction to board

--- Assets/Scripts/Game/BoardBase/Board.cs
+++ Assets/Scripts/Game/BoardBase/Board.cs
@@ -1,3 +1,5 @@
+using System;
+
 using UnityEngine;
 using Zenject;
 
@@ -6,9 +8,9 @@ public class Board : MonoBehaviour
     [SerializeField] private Transform cellParent;
     [Inject] private Cell.CellFactory _cellFactory;
     [Inject] private SignalBus _signalBus;
-    public int Rows {  get; private set; }
+    public int Rows { get; private set; }
     public int Cols { get; private set; }
-    public Cell[,] Cells {  get; private set; }
+    public Cell[,] Cells { get; private set; }
 
     private void Awake()
     {
@@ -16,14 +18,14 @@ public class Board : MonoBehaviour
     }
     private void OnDestroy()
     {
-        _signalBus.Unsubscribe < OnElementTappedSignal>(CellTapped);
+        _signalBus.Unsubscribe<OnElementTappedSignal>(CellTapped);
     }
 
-    public void Prepare(int row,int col)
+    public void Prepare(int row, int col)
     {
         Rows = row;
         Cols = col;
-        Cells=new Cell[Rows, Cols];
+        Cells = new Cell[Rows, Cols];
 
         CreateCells();
         PrepareCells();
@@ -31,8 +33,62 @@ public class Board : MonoBehaviour
 
     private void CellTapped(OnElementTappedSignal signal)
     {
-        var cell= signal.Touchable.gameObject.GetComponent<Cell>();
+        var cell = signal.Touchable.gameObject.GetComponent<Cell>();
     }
+
+
+    private Cell GetNeighborWithDirection(Cell cell, Directions direction)
+    {
+        var x = cell.X;
+        var y = cell.Y;
+
+        switch (direction)
+        {
+            case Directions.None:
+                break;
+            case Directions.Up:
+                y += 1;
+                break;
+            case Directions.UpRight:
+                y += 1;
+                x += 1;
+                break;
+            case Directions.Right:
+                x += 1;
+                break;
+            case Directions.DownRight:
+                y -= 1;
+                x += 1;
+                break;
+            case Directions.Down:
+                y -= 1;
+                break;
+            case Directions.DownLeft:
+                y -= 1;
+                x -= 1;
+                break;
+            case Directions.Left:
+                x -= 1;
+                break;
+            case Directions.UpLeft:
+                y += 1;
+                x -= 1;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+
+
+        }
+        if (x < 0 || x >= Rows || y < 0 || y >= Cols)
+        {
+            return null;
+        }
+
+        return Cells[x, y];
+    }
+
+
+
 
     private void CreateCells()
     {
