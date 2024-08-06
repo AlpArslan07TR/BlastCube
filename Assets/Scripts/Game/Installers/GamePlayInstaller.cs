657cf61e9312bed94ac068eb8c1e2b048741c424
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-08-06 22:53:33 +0300

    Update GamePlayInstaller.cs

--- Assets/Scripts/Game/Installers/GamePlayInstaller.cs
+++ Assets/Scripts/Game/Installers/GamePlayInstaller.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 using Zenject;
 
-[CreateAssetMenu(fileName = "GamePlayInstaller", menuName = "Installers/GamePlayInstaller")]
-public class GamePlayInstaller : ScriptableObjectInstaller<GamePlayInstaller>
+[CreateAssetMenu(fileName = "GameplayInstaller", menuName = "Installers/GameplayInstaller")]
+public class GameplayInstaller : ScriptableObjectInstaller<GameplayInstaller>
 {
     [SerializeField] private Cell cellPrefab;
-    [SerializeField] private ItemBase itemPrefab;
+    [SerializeField] private ItemBase itemBasePrefab;
+    [SerializeField] private Item itemPrefab;
+
     public override void InstallBindings()
     {
         SignalBusInstaller.Install(Container);
-        Container.Bind<ImageLibService>().FromComponentInHierarchy().AsSingle();
-        Container.Bind<Board>().FromComponentInHierarchy().AsSingle();
-        Container.BindFactory<Cell,Cell.CellFactory>().FromComponentInNewPrefab(cellPrefab).AsSingle();
-        Container.BindFactory<ItemBase, ItemBase.Factory>().FromComponentInNewPrefab(itemPrefab).AsSingle();
-        Container.BindFactory<Item, Item.Factory>().FromComponentInNewPrefab(itemPrefab).AsSingle();
+
+        Container.Bind<Board>().FromComponentInHierarchy()
+            .AsSingle();
+
+        Container.Bind<ImageLibService>().FromComponentInHierarchy()
+            .AsSingle();
+
+        Container.BindFactory<Cell, Cell.CellFactory>()
+            .FromComponentInNewPrefab(cellPrefab)
+            .AsSingle();
+
+        Container.BindFactory<ItemBase, ItemBase.Factory>()
+            .FromComponentInNewPrefab(itemBasePrefab)
+            .AsSingle();
+
+        Container.BindFactory<Item, Item.Factory>()
+            .FromComponentInNewPrefab(itemPrefab)
+            .AsSingle();
+
         Container.DeclareSignal<OnElementTappedSignal>();
         Container.DeclareSignal<OnEmptyTappedSignal>();
     }
