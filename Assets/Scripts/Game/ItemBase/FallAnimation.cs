c722b3b8700ad023ecbe8aeb81efcabd80f007ec
Alparslan Yurtseven <m.alparslan03@hotmail.com>
2024-08-13 20:40:29 +0300

    added Fall anim calculations

--- Assets/Scripts/Game/ItemBase/FallAnimation.cs
+++ Assets/Scripts/Game/ItemBase/FallAnimation.cs
@@ -22,6 +22,29 @@ public class FallAnimation : MonoBehaviour
         {
             TargetCell = targetCell;
             item.cell = targetCell;
+            _targetPos = TargetCell.transform.position;
+            IsFalling = true;
         }
+    }
+
+    private void Update()
+    {
+        if (!IsFalling) return;
+
+        _currentVel += _itemStatsSO.acc;
+        _currentVel = _currentVel >= _itemStatsSO.maxSpeed ? _itemStatsSO.maxSpeed : _currentVel;
+
+        var p = item.transform.position;
+        p.y = _currentVel * Time.deltaTime;
+
+        if(p.y<=_targetPos.y)
+        {
+            IsFalling = false;
+            TargetCell = null;
+            p.y = _targetPos.y;
+            _currentVel = _itemStatsSO.startVel;
+        }
+
+        item.transform.position = p;
     }
 }
